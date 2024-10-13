f68ffe28386da5c3389dd6790e9ddaa0cf6b31c0
singss-hash <singssp03ragya@gmail.com>
2024-10-13 22:34:24 +0530

    Delete springboot-backend directory

--- springboot-backend/src/main/java/net/java/springboot/repository/CompanyOrgRepository.java
+++
@@ -1,9 +0,0 @@
-package net.java.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.java.springboot.entity.CompanyOrg;
-
-public interface CompanyOrgRepository extends JpaRepository<CompanyOrg,Long> {
-
-}
